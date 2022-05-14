0bed4cb96f333ca3ab6af1ddf73818aee5f73c3d
Kevinwijesooriya <81944276+Kevinwijesooriya@users.noreply.github.com>
2022-05-14 13:46:56 +0530

    user service initial implementations

---
+++ services/user-service/models/user.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default model("Users", userSchema);
