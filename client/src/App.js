75ca26f46f824fbd715f48fee8a8fb635b3210d1
Kevinwijesooriya <81944276+Kevinwijesooriya@users.noreply.github.com>
2022-05-16 19:09:14 +0530

    Folder restructure

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import MovieCard from './components/Cards/MovieCard';
+import ViewMovies from './components/movie/ViewMovies';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          App
-        </p>
-      </header>
+      <h1>Welcome to App!</h1>
+      <Link to="/movie-view" >Home</Link>
+      <Routes>
+        <Route path="/" element={<MovieCard />} />
+        <Route path="movie-view" element={<ViewMovies />} />
+      </Routes>
     </div>
   );
 }
