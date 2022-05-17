49095dbc8845996cda4a62b89d084f7ebdc42105
amila1998 <amiladevin@gmail.com>
2022-05-17 05:31:38 +0530

    FE Modifications

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div><div class="footer-content">
+    <h3>Foolish Developer</h3>
+    <p>Raj Template is a blog website where you will find great tutorials on web design and development. Here each tutorial is beautifully described step by step with the required source code.</p>
+    <ul class="socials">
+        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+        <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+        <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+    </ul>
+</div>
+<div class="footer-bottom">
+    <p>copyright &copy; <a href="#">Foolish Developer</a>  </p>
+            <div class="footer-menu">
+              <ul class="f-menu">
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+                <li><a href="">Blog</a></li>
+              </ul>
+            </div>
+</div></div>
+  )
+}
+
+export default Footer
--- client/src/components/MainPages.jsx
+++ client/src/components/MainPages.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import ViewMovies from './movie/ViewMovies.js';
 import MovieCard from './Cards/MovieCard.js';
+import NotFound from './utils/not_found/NotFound.jsx';
+import Login from './Login/Login.jsx';
 
 
 const MainPages = () => {
+    const isLogged = false;
   return (
          
         <Routes>
               <Route path="/" element={<MovieCard />} />
-              <Route path="movie-view" element={<ViewMovies />} />
+              <Route path="/movie-view" element={<ViewMovies />} />
+              <Route path="/login" element={isLogged ? <Navigate to={"/"}/> : <Login/>} />
         </Routes>
 
   )
