966b666f68cecd6fa69150d842dee443a86ba3ca
amila1998 <amiladevin@gmail.com>
2022-05-26 00:02:33 +0530

    Email Send

--- services/ticket-service/controllers/paymentController.js
+++ services/ticket-service/controllers/paymentController.js
@@ -1,11 +1,16 @@
+import sendEmail from '../helpers/sendMail.js';
 import Payments from '../models/payment.js';
 import Tickets from '../models/ticket.js';
+import User from '../models/user.js';
 
 const paymentController ={
     createPayment: async(req, res) => {
         try {
            
             const {ticket_id, total} = req.body;
+            const user = await User.findById(req.user.id).select('-password')
+            if(!user) return res.status(400).json({msg: "User does not exist."})
+            const email=user.email;
 
 
 
@@ -13,7 +18,7 @@ const paymentController ={
                 ticket_id , total
             })
 
-            
+            sendEmail(email, ticket_id,total);
             await newPayment.save();
             await Tickets.findOneAndUpdate({_id: ticket_id}, {
                 isPaid: true
