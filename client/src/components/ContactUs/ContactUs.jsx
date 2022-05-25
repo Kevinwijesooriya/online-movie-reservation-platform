6b0f71b337c87213fd067461bb9182b4926d667d
Sithara-tech <sitharakumarasingha@gmail.com>
2022-05-25 23:17:29 +0530

    Contact Us

--- client/src/components/ContactUs/ContactUs.jsx
+++ client/src/components/ContactUs/ContactUs.jsx
@@ -1,36 +1,71 @@
-import React from 'react'
+import React, { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
+import "./ContactUs.css";
 
+const Result = () => {
+    return (
+        <p>Your message has been successfully sent. I will contact you soon</p>
+    );
+};
 
+function ContactUs() {
+    const form = useRef();
+    const [result, showResult] = useState(false);
+    const sendEmail = (e) => {
+        e.preventDefault();
 
-function ContactUs () {
- 
-   return(
-       <form action="" onSubmit="">
-           <div className='forward'>
-               <h2>Contact Us</h2>
-               <span>Full name</span>
-               < br/>
-               <input className="input100"
-               type="text" name="fullname" required
-               />
-               <br />
-               <span>Phone number</span>
-               < br/>
-               <input className="input100"
-               type="text" name="phone" required
-               />
-               < br/>
-               <span>Email</span>
-               < br/>
-               <input className="input100"
-               type="text" name="email" required
-               />
-               < br/>
+        emailjs
+            .sendForm(
+                "service_xr06ow9",
+                "template_ii74t46",
+                form.current,
+                "cZ1_wHzdL9S5pgh0H"
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
+        e.target.reset();
+        showResult(true);
+    };
 
-           </div>
-       </form>
-   )
-  
+    setTimeout(() => {
+        showResult(false)
+    }, 5000);
+
+    return (<div className="form">
+        <form className="" controlId="formBasicEmail" ref={form} action="" onSubmit={sendEmail}>
+            <div className="form-group">
+                <h2 className="brand-title">Contact Us</h2><div className="inputs">
+                    <span>Full name</span>
+                    <br />
+                    <input className="input100" type="text" name="fullName" required />
+                    <br />
+                    <span>Phone number</span>
+                    <br />
+                    <input className="input100" type="text" name="phone" required />
+                    <br />
+                    <span>Email</span>
+                    <br />
+                    <input className="input100" type="text" name="email" required />
+                    <br /></div>
+                <span>Message</span>
+                <br />
+                <textarea name="message" required></textarea>
+
+                <div>
+
+                    
+                    <button className="sub">Submit</button>
+
+                    <div className="row">{result ? <Result /> : null}</div>
+                </div></div>
+        </form></div>
+    );
 }
 
 export default ContactUs;
--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-<div class="footer-bottom  position-absolute">
-    <p>copyright &copy; <a href="https://www.sliit.lk/">2022S1_REG_31 @ SLIIT</a>  </p>
-            <div class="footer-menu">
-              <ul class="f-menu">
-                <li><a href="">Home</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Contact</a></li>
-                <li><a href="">Blog</a></li>
-              </ul>
-            </div>
-</div></div>
+      <div class="footer-bottom  position-absolute">
+        <p>copyright &copy; <a href="https://www.sliit.lk/">2022S1_REG_31 @ SLIIT</a>  </p>
+        <div class="footer-menu">
+          <ul class="f-menu">
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="/ContactUs">Contact</a></li>
+            <li><a href="">Blog</a></li>
+          </ul>
+        </div>
+      </div></div>
   )
 }
 
